a99145dde5ef9b9766de7169014d391049116549
abhisekthapam <ahisekmagar250203@gmail.com>
2024-12-16 21:51:35 +0545

    feat:2 User login post request functionality  with JWT token and store login history

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 
@@ -13,6 +14,7 @@ app.use(cors());
 
 connectDB();
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
