99d10741a4678b8702431f33b823dc7f7eb1bb92
Abhisek Thapa <112496493+abhisekthapam@users.noreply.github.com>
2025-01-29 20:19:09 +0545

    feat:6 Admin only put request to update users by ID (#6)
    Co-authored-by: abhisekthapam <ahisekmagar250203@gmail.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { registerUser, getAllUsers, getUserById } = require('../controllers/userController');
+const { registerUser, getAllUsers, getUserById, updateUser } = require('../controllers/userController');
 const router = express.Router();
 const { verifyTokenAndAdmin } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
+
 router.get('/all', verifyTokenAndAdmin, getAllUsers);
 router.get('/:id', verifyTokenAndAdmin, getUserById);
+router.put('/:id', verifyTokenAndAdmin, updateUser);
 
 module.exports = router;
