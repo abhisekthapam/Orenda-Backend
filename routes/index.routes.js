d91c0d6de5971704344704f879f573f7e128dc2b
abhisekthapam <ahisekmagar250203@gmail.com>
2025-02-28 19:26:40 +0545

    feat : 10 Order routes

---
+++ routes/index.routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const upload = require("../utils/file.upload");
+const userRoutes = require("./user.routes");
+const menuRoutes = require("./menu.routes");
+const orderRoutes = require("./order.routes");
+const imageController = require("../controller/image.controller");
+
+router.use("/user", userRoutes);
+router.use("/menu", menuRoutes);
+router.use("/order", orderRoutes);
+
+router.post("/upload", upload.single("image"), imageController.createImage);
+router.put(
+  "/updateImage/:id",
+  upload.single("image"),
+  imageController.updateImage
+);
+module.exports = router;
---
+++ routes/order.routes.js
@@ -0,0 +1,11 @@
+const verify = require("../middleware/token.verify");
+const router = require("express").Router();
+const orderController = require("../controller/order.controller");
+router.get("/all", verify("admin"), orderController.getOrders);
+router.get("/calc", verify("admin"), orderController.getAllOrder);
+router.get("/:year/:month", verify("admin"), orderController.getOrdersByMonth);
+router.get("/:tableId", orderController.getOrderByTableId);
+router.post("/", orderController.createOrder);
+router.put("/:orderId", verify("admin"), orderController.updateOrderStatus);
+
+module.exports = router;
